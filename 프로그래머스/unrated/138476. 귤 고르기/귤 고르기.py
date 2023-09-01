c124f8b4df4d99f21a5290387a4a5c7471c02be8
bone0825 <88430215+bone0825@users.noreply.github.com>
2023-09-01 11:57:48 +0900

    [unrated] Title: 귤 고르기, Time: 77.57 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,21 +1,15 @@
-from collections import Counter
-
 def solution(k, tangerine):
+    arr = [0]* (max(tangerine)+1)
+    temp1 = 0
     answer = 0
-    counter = Counter(tangerine)
-    sort_count = sorted(counter.items(), key = lambda item: item[1], reverse = True)
     
-    for i in sort_count:
-        if k <= 0:
-            return answer
-        k -= i[1]
-        answer +=1
-        """
-    while(list(counter.values())[answer] < k):
-        k= k - list(counter.values())[answer]
+    while tangerine:
+        temp = tangerine.pop()
+        arr[temp] += 1
+        
+    arr.sort()
+    while k > temp1:
+        temp1 += arr.pop()
         answer += 1
-    
-    if(k != 0 and answer == 0):
-        answer += 1
-        """
+        
     return answer
