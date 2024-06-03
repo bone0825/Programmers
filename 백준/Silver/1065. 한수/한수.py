d6ae3da92d6b3a9ac9c1ad57eb2c1ea17c9ee2b2
bone0825 <88430215+bone0825@users.noreply.github.com>
2024-06-03 19:19:56 +0900

    [Silver IV] Title: 한수, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -7,8 +7,12 @@ else:
     check = 99
     T = int(T)
 
-    for i in range(100,T + 1):
-        i = str(i)
-        if ((int(i[0]) + int(i[2]))/ 2 == int(i[1])):
+    for i in range(100, T + 1):
+        temp = []
+        while (i):
+            temp.append(i % 10)
+            i = i // 10
+        if ((temp[-1] - temp[-2]) == (temp[-2] - temp[-3])):
             check += 1
     print(check)
+
